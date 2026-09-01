1d228fe43ac0b2b542bfd6bf6baee175c7f0999b
Alexandr Nedvedicky <sashan@openssl.org>
2026-09-02 01:02:30 +0200

    - fix becnh-range.py

--- bench-range.py
+++ bench-range.py
@@ -4,14 +4,26 @@ suite = 'AES128'
 
 print 'len,send,recv'
 
-for len in [16, 32, 64, 128, 512, 1024, 4096, 8192, 32768, 65536, 131072, 262144, 1048576]:
-    out = subprocess.check_output(['./bench', 'bulk', suite, str(len)])
-    lines = out.splitlines()
+openssl_forks = [
+    './bench-aws-lc',
+    './bench-boringssk',
+    './bench-libressl',
+    './bench-openssl',
+    './bench-wolfssl',
+]
 
-    for l in out.splitlines():
-        if l.startswith('send: '):
-            send = float(l.split()[1])
-        if l.startswith('recv: '):
-            recv = float(l.split()[1])
+for bench in openssl_forks:
+    print "###"
+    print "# %s" % (bench)
+    print "###"
+    for len in [16, 32, 64, 128, 512, 1024, 4096, 8192, 32768, 65536, 131072, 262144, 1048576]:
+        out = subprocess.check_output([bench, 'bulk', suite, str(len)])
+        lines = out.splitlines()
 
-    print '%d,%g,%g' % (len, send, recv)
+        for l in out.splitlines():
+            if l.startswith('send: '):
+                send = float(l.split()[1])
+            if l.startswith('recv: '):
+                recv = float(l.split()[1])
+
+        print '%d,%g,%g' % (len, send, recv)
